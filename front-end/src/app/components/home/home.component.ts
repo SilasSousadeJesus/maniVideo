f9bfc7ae842f20a5cdd5d03017fb496fff565d16
Silas Sousa <silassousadejesus@gmail.com>
2022-05-22 14:45:10 -0300

    implementando reactiveforms

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicesService } from "../services/services.service";
-import { FormBuilder } from '@angular/forms';
-import { DomSanitizer, SafeUrl, SafeHtml, SafeValue } from "@angular/platform-browser";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DomSanitizer } from "@angular/platform-browser";
+
 
 @Component({
   selector: 'app-home',
@@ -12,14 +13,14 @@ export class HomeComponent implements OnInit {
 
   formVideo = this.formBuilder.group({
 
-    urlYt:[''],
-    qualityVideo:['']
+    urlYt:['', Validators.required],
+    qualityVideo:['', Validators.required]
 
   })
 
   formMP3 = this.formBuilder.group({
 
-    urlYt:['']
+    urlYt:['', Validators.required]
 
   })
 
@@ -34,20 +35,24 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  video(){
-    this.service.downloadvideo(this.formVideo.value).subscribe(res=>{
 
-    }, err=>console.log)
-  }
+  //  video(){
+  //    this.service.downloadvideo(this.formVideo.value).subscribe(res=>{
+  //       this.formVideo = res
+  //    }, err=>console.log)
+  //  }
 
   securefullurl(){
     this.fullUrl = this._sanitizer.sanitize(4, `https://www.youtube.com/embed/${this.videoId}`)
     return this.fullUrl
   }
 
+
+
   saveForm(){
-    console.log(this.formVideo.value)
     console.log(this.fullUrl)
+    this.formVideo.controls['urlYt'].setValue(this.securefullurl())
+    console.log(this.formVideo.value)
   }
 
   // https://www.youtube.com/watch?v=letmVxvyvig
